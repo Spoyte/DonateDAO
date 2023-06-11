94f94845700f76890ab8ca02e9febcc554bcba64
Spoyte <104215259+Spoyte@users.noreply.github.com>
2023-06-11 02:09:13 +0200

    Update use-paymaster-3.ts

--- zksync/paymaster-dapi/deploy/use-paymaster-3.ts
+++ zksync/paymaster-dapi/deploy/use-paymaster-3.ts
@@ -9,6 +9,7 @@ require("dotenv").config();
 // Put the address of the deployed paymaster and the Greeter Contract in the .env file
 const PAYMASTER_ADDRESS = process.env.PAYMASTER_ADDRESS;
 const EMPTY_WALLET_ADDRESS = process.env.EMPTY_WALLET_ADDRESS;
+// value of usdc to send, 18 decimals
 const VALUE_TO_SEND = process.env.VALUE_TO_SEND;
 
 // Put the address of the ERC20 token in the .env file:
